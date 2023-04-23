8ecef2b425cc055e16708da136e2dcf5ca62a3dc
benrumalupet <sapornera@students.nu-baliwag.edu.ph>
2023-04-24 00:24:30 +0800

    update emil

--- response.php
+++ response.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>phpEmail</title>
+  <title>Feedback</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/rizalcss@1.5.0/css/rizal.min.css" integrity="sha256-c+mRP5IEpihf3MvgbkG1cScBdbRoVQCW9PiGmS7uFA8=" crossorigin="anonymous">
   <script defer src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
   <script defer src="https://kit.fontawesome.com/1e8d61f212.js"></script>
